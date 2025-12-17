b93701cb11a9f4ba2221291d9eae716b9dca27ec
LuposX <36456825+LuposX@users.noreply.github.com>
2025-12-17 12:31:55 +0100

    fixed sweep spiking

--- surrogate/4_train_non_spiking_surrogate.py
+++ surrogate/4_train_non_spiking_surrogate.py
@@ -320,7 +320,6 @@ def main(args):
             num_hidden_layers=int(local_args.get("num_hidden_layers")),
             num_hidden=int(local_args.get("num_hidden")),
             rnn_type=local_args.get("rnn_type"),
-            num_static_params=local_args.get("num_static_params"),
             optimizer_class=local_optimizer_class,
             optimizer_kwargs=local_optimizer_kwargs,
             lr=float(local_args.get("lr")),
